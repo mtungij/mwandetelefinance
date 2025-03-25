727a52de21f34dad2a713c914cf758b39fcd0c33
mtungij <jamesmtungi2013@gmail.com>
2025-03-26 00:54:24 +0300

    hello

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost:8013/'; 
-// $config['base_url'] = 'https://mwandetelefinance.phamasoft.com/'; 
+// $config['base_url'] = 'http://localhost:8013/'; 
+$config['base_url'] = 'https://mwandetelefinance.phamasoft.com/'; 
 /*
 |--------------------------------------------------------------------------
 | Index File
